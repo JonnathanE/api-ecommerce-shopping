180808b7d568380587978697f6c34166b1e4b985
JonnathanE <jonnathan.espinoza@unl.edu.ec>
2022-01-10 22:46:40 -0500

    add functionality to get user statistics

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { update, deleteById, getUserById, getAllUsers } = require('../controllers/user.controller');
+const { update, deleteById, getUserById, getAllUsers, stats } = require('../controllers/user.controller');
 const { verifyTokenAndAuthorization, verifyTokenAndAdmin } = require('../middlewares/authJwt');
 
 // UPDATE
@@ -7,8 +7,10 @@ router.put('/:id', verifyTokenAndAuthorization, update);
 // DELETE
 router.delete('/:id', verifyTokenAndAuthorization, deleteById);
 // GET USER BY ID
-router.get('/:id', verifyTokenAndAdmin, getUserById);
+router.get('/find/:id', verifyTokenAndAdmin, getUserById);
 // GET ALL USERS
 router.get('/', verifyTokenAndAdmin, getAllUsers);
+// USER STATS
+router.get('/stats', verifyTokenAndAdmin, stats);
 
 module.exports = router;
